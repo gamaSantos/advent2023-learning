0095fd2c37cd3d8ced442a1f73002036c6556c8d
Pedro Santos <pedro@logstore.com.br>
2023-12-16 22:23:18 -0300

    fix: remove home address

--- advent_02/src/main.rs
+++ advent_02/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
+    let file_path = "~/projects/advent2023-learning/advent_02/test.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let lines = contents.split('\n');
     println!("Hello, world!");
 }
